1d81c3cec487893f708a445b43a9782ca5ee9e50
davidemecugni <299046@studenti.unimore.it>
2023-03-19 17:45:04 +0100

    Modified README.md

--- GameOfLife/Settings.py
+++ GameOfLife/Settings.py
@@ -5,16 +5,13 @@ from pygame import Color, image
 class Settings:
     """Stores all the settings from AI"""
     def __init__(self):
-        self.screen_width = 1000
+        self.screen_width = 800
         self.screen_height = self.screen_width
-        self.columns = 100
+        self.columns = 80
         self.rows = self.columns
         self.width_of_squares = self.screen_width//self.columns
         self.height_of_squares = self.width_of_squares
         self.caption = "Game of life"
-
-        #Optimization
-        self.interval_draw = 1
         self.pacman = False #Pacman universe
         # States
         self.start = None
